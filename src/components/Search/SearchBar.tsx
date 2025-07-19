8e165cc6468f2fb8d98c816e2e9ff2c5f286e96e
Sergey Kulchytski <workintellect@gmail.com>
2025-07-19 22:01:26 +0300

    feat: add SearchBar test

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Button from '../Button.tsx';
 
 interface Props {
   query: string;
@@ -13,18 +14,20 @@ class SearchBar extends Component<Props> {
     return (
       <div className="w-full flex py-8 justify-center gap-x-4">
         <input
+          data-testid="search-input"
           className="w-full max-w-md px-4 py-2 border border-gray-300 outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400"
           type="text"
           value={query}
           onChange={onChange}
           placeholder="Enter a request, e.g. C-3PO"
         />
-        <button
+        <Button
+          data-testid="search-button"
           onClick={onSearch}
-          className="border border-gray-300 py-2 px-4 hover:bg-gray-50"
+          variant="default"
         >
           Search
-        </button>
+        </Button>
       </div>
     );
   }
