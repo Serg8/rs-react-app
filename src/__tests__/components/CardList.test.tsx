f22b0acdcc19d3cc04a3215b524134600120f08c
Sergey Kulchytski <workintellect@gmail.com>
2025-07-19 14:03:50 +0300

    feat: add CardList test for correct number of items rendered

---
+++ src/__tests__/components/CardList.test.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import CardList from '../../components/CardList.tsx';
+
+const mockResults = [
+  {
+    name: 'Luke Skywalker',
+    gender: 'male',
+    height: '172',
+    mass: '77',
+    birth_year: '19BBY',
+    eye_color: 'blue',
+    hair_color: 'blond',
+  },
+  {
+    name: 'Leia Organa',
+    gender: 'female',
+    height: '150',
+    mass: '49',
+    birth_year: '19BBY',
+    eye_color: 'brown',
+    hair_color: 'brown',
+  },
+];
+
+describe('CardList component', () => {
+  test('Renders correct number of items when data is provided', () => {
+    render(<CardList results={mockResults} />);
+    const cards = screen.getAllByTestId('card');
+    expect(cards).toHaveLength(mockResults.length);
+  });
+});
